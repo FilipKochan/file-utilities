22577486d6293185aae2ce31a0b441b7d1c03d98
FilipKochan <filda.koch@gmail.com>
2023-01-26 11:09:30 +0100

    added test for LastUploadedFileSelectorTest.php

--- tests/LastUploadedFileSelectorTest.php
+++ tests/LastUploadedFileSelectorTest.php
@@ -6,7 +6,41 @@ use PHPUnit\Framework\TestCase;
 
 class LastUploadedFileSelectorTest extends TestCase
 {
-    public function testTrue() {
-        self::assertTrue(true);
+    private const dir = __DIR__.'/tmp/uploads';
+    private const prefix = 'f';
+    private LastUploadedFileSelector $lastUploadedFileSelector;
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->lastUploadedFileSelector = new LastUploadedFileSelector(self::prefix);
+    }
+
+
+    public static function setUpBeforeClass(): void {
+        mkdir(self::dir, recursive: true);
+
+        touch(self::dir."/f_2022-09-04T11:06:22+01:00");
+        touch(self::dir."/f_2023-01-26T11:06:22+01:00");
+        touch(self::dir."/f_2023-01-20T11:06:22+01:00");
+        touch(self::dir."/f_2021-04-04T11:06:22+01:00");
+    }
+
+    public function testCanChooseLastFile() {
+        self::assertEquals('f_2023-01-26T11:06:22+01:00', $this->lastUploadedFileSelector->get_filename(self::dir));
+    }
+
+    public static function tearDownAfterClass(): void {
+        $d = opendir(self::dir);
+        try {
+            while (($f = readdir($d))) {
+                if (is_file(self::dir.'/'.$f)) {
+                    unlink(self::dir.'/'.$f);
+                }
+            }
+            rmdir(self::dir);
+            rmdir(dirname(self::dir));
+        } finally {
+            closedir($d);
+        }
     }
 }
